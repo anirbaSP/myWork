e5d8a7d373c71189f863f38bc71b9993b38edec6
Pei Sabrina Xu <psxu@firefly.inscopix.com>
2018-01-16 16:04:56 -0800

    play with matplotlib

--- dualColor/testMosaic2PyAPI.py
+++ dualColor/testMosaic2PyAPI.py
@@ -1,4 +1,5 @@
 import isx
+
 import numpy as np
 
 import matplotlib
@@ -13,20 +14,45 @@ from os.path import isfile, join
 # always initialize the API before use
 isx.initialize()
 
+# input files
 root_dir = '/ariel/data2/Alice/NV3_DualColor/NV3_color_sensor_12bit/V3-17/20170717/pipeline'
 fn = [f for f in listdir(root_dir) if isfile(join(root_dir, f))]
 
-## find the files for RGB channels
-# get channel tag from file name
-tag = list()
-for i in range(len(fn)):
-    print(fn[i])
-    name, ext = os.path.splitext(fn[i])
-    idx1 = name.rfind('_')
-    print(name[idx+1:-1])
-    # tag[i] = name[idx]
+print('{} files have been found, they are \n {}'.format(len(fn), fn))
 
-# recording_file = os.path.join(root_dir, 'Movie_2017-07-17-10-39-25_red-PP.isxd')
+# find the files for RGB channels
+ch = list()
+for f in fn:
+    # name, ext = os.path.splitext(fn[i])
+    idx1 = f.rfind('_')
+    idx2 = f.rfind('-')
+    color = f[idx1+1:idx2]
+    ch.append(color)
+
+print('{} channels have been found, they are {}'.format(len(ch), ch))
+
+
+# show example frame for each channel
+plt.figure()
+count = 0
+for f in fn:
+    this_file = os.path.join(root_dir, f)
+    this_movie = isx.Movie(this_file)
+
+    print('Frame rate:{:0.2f} Hz'.format(this_movie.frame_rate))
+    print('#of frames:{}'.format(this_movie.num_frames))
+    print('Movie size: {} rows by {} colums'.format(this_movie.shape[0], this_movie.shape[1]))
+
+    this_frame0 = this_movie.read_frame(0)
+    # fig = plt.figure()
+
+    fig.suptitle('{}'.format(ch[count]))
+    plt.imshow(this_frame0, aspect='auto')
+    plt.show()
+
+    count += 1
+# recording_file = os.path.join(root_dir, fn)  # 'Movie_2017-07-17-10-39-25_red-PP.isxd'
+
 #
 # # for later use, grab the name of the recording file, and strip off the file extension
 # root_dir, recording_file_name_with_ext = os.path.split(recording_file)
--- dualColor/tmp.py
+++ dualColor/tmp.py
@@ -4,8 +4,12 @@ Created on Jan 11, 2018
 @author: psxu
 '''
 
-import isx
+import matplotlib.pyplot as plt
 
-root_dir = '/ariel/data2/Alice/NV3_DualColor/NV3_color_sensor_12bit/V3-17/20170717'
+plt.figure()
 
-print(root_dir)
+plt.plot([1, 2, 3])
+
+# plt.subplot(211)
+# plt.plot(range(12))
+# plt.subplot(212, facecolor='y')
